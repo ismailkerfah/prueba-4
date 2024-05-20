258b459c36b873edf1ce0ff6346078803d348d88
ismailkerfah <ismailkerfah>
2024-05-20 17:25:59 +0200

    prueba 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,29 @@
 plugins {
-    id("com.google.gms.google-services") version "4.4.1" apply false
+  // ...
+
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.1" apply false
+
+}
+plugins {
+  id("com.android.application")
+
+  // Add the Google services Gradle plugin
+  id("com.google.gms.google-services")
+
+  ...
 }
 
-// Otras configuraciones de tu archivo build.gradle.kts
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
+}
